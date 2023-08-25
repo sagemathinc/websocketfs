71906754c468ec822b05a553b338e2259a2a4cfb
William Stein <wstein@sagemath.com>
2023-08-25 22:30:23 +0000

    fix a package export

--- websocketfs/lib/serve.ts
+++ websocketfs/lib/serve.ts
@@ -8,29 +8,33 @@ export default async function serve({
   path,
   host = "localhost",
   port,
+  noServer,
   options,
 }: {
   path: string;
   host?: string;
   port?: number;
+  noServer?: boolean;
   options?: any;
-}): Promise<{ port: number; server: SftpServer }> {
-  if (port == null) {
+}): Promise<{ port?: number; server: SftpServer }> {
+  if (port == null && !noServer) {
     // get an available port
     port = await getPort();
-  }
-  if (port == null) {
-    throw Error("bug");
   }
 
   // start SFTP server on localhost
   const server = new SftpServer({
     ...options,
+    noServer,
     host,
     port,
     virtualRoot: path,
   });
-  log("SFTP server listening on port ", port);
+  if (!noServer) {
+    log("Created Sftp server listening on port ", port);
+  } else {
+    log("Created Sftp websocket server but no http server");
+  }
 
   return { port, server };
 }
